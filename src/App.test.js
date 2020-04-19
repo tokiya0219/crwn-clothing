95fb6ce77d6a946e97060cc6e371685c93c1d17a
tokiya0219 <53889976+tokiya0219@users.noreply.github.com>
2020-04-19 19:53:37 +0900

    implemented firebase utils, included abilities to store authenticated user into our firestore database

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+const firestore = firebase.firestore();
+
+firestore.collection('users').doc('RJZdSz3OYJ95MQ7JABjN').collection('cartItems').doc('Y9dqo3G7RxYZgqHkCp2y');
+firestore.doc('/users/RJZdSz3OYJ95MQ7JABjN/cartItems/Y9dqo3G7RxYZgqHkCp2y');
+firestore.collection('/users/RJZdSz3OYJ95MQ7JABjN/cartItems');
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -54,9 +54,7 @@ class SignIn extends React.Component {
           />
           <div className='buttons'>
             <CustomButton type='submit'> Sign in </CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-              Sign in with Google
-            </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>sign in with Google</CustomButton>
           </div>
         </form>
       </div>
