95459dfa3147c3a609d4a890020d2203d34e4ee0
tokiya0219 <53889976+tokiya0219@users.noreply.github.com>
2020-05-24 23:30:54 +0900

    creating new containers is store the actions we use for the components which is match the name file

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,32 @@
 import ShopActionTypes from './shop.types';
 
-export const updateCollections = collectionsMap => ({
-    type: ShopActionTypes.UPDATE_COLLECTIONS,
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils'
+
+export const fetchCollectionsStart = () => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_START
+});
+
+export const fetchCollectionsSuccess = collectionsMap => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
     payload: collectionsMap
 });
+
+export const fetchCollectionsFailure = errorMessage => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+    payload: errorMessage
+});
+
+export const fetchCollectionsStartAsync = () => {
+    return dispatch => {
+        const collectionRef = firestore.collection('collections');
+        dispatch(fetchCollectionsStart());
+        // fetch('https://firestore.googleapis.com/v1/projects/crown-db-f61c5/databases/(default)/documents/collections')
+        // .then(response => response.json())
+        // .then(collections => console.log(collections));
+
+        collectionRef.get().then(snapshot => {
+            const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+            dispatch(fetchCollectionsSuccess(collectionsMap));
+    }).catch(error => dispatch(fetchCollectionsFailure(error.message)));
+    }
+}
